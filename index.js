ee58cd700021b577bb9bd3e33c4b7836eafdf981
noalbertin <jeanneaurelle100@gmail.com>
2025-05-09 11:28:09 +0300

    Modification 1

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const etudiantsRoute = require("./routes/etudiants");
+const port = process.env.PORT || 3000;
 
-app.use(cors());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use("/etudiants", etudiantsRoute);
 
-app.listen(5000, '0.0.0.0', () => {
-    console.log("API en marche sur http://0.0.0.0:5000");
+app.listen(port, '0.0.0.0', () => {
+    console.log(`API en marche sur http://0.0.0.0:${port}`);
 });
+
   
